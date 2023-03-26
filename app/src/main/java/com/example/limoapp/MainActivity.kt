c9252064e696fec3fe153d2c18159244a57cbe96
Faezol <fpcyber61@gmail.com>
2023-03-27 05:55:51 +0800

    Allow permision for akses storage

--- app/src/main/java/com/example/limoapp/MainActivity.kt
+++ app/src/main/java/com/example/limoapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.limoapp
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.limoapp.adapter.ListMomentAdapter
@@ -10,17 +13,70 @@ import com.example.limoapp.databinding.ActivityMainBinding
 import com.example.limoapp.ui.addmoment.AddMomentActivity
 import com.example.limoapp.utils.ViewModelFactory
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
     private lateinit var adapter: ListMomentAdapter
+    private val PERMISSIONS_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //VIEWMODEL
+
+        // Memeriksa apakah aplikasi memiliki izin akses
+        if (checkSelfPermission(android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED || checkSelfPermission(
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+
+            // Jika tidak memiliki izin akses, maka meminta izin akses
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), PERMISSIONS_REQUEST_CODE
+            )
+
+        } else {
+
+            // Jika sudah memiliki izin akses, maka menjalankan kode utama
+            firstSet()
+
+        }
+    }
+
+    // Menangani hasil dari permintaan izin akses
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // Jika izin akses diberikan, maka menjalankan kode utama
+                firstSet()
+
+            } else {
+
+                // Jika izin akses tidak diberikan, maka menampilkan pesan
+                Toast.makeText(
+                    this, "Izin akses diperlukan untuk menjalankan aplikasi", Toast.LENGTH_SHORT
+                ).show()
+
+                // Menutup aplikasi
+                finish()
+            }
+        }
+    }
+
+    private fun firstSet(){
+        // VIEWMODEL
         val factory = ViewModelFactory.getInstance(this.applicationContext)
         viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
+
         //ADAPTER
         adapter = ListMomentAdapter()
         binding.rvComment.layoutManager = LinearLayoutManager(this)
@@ -40,4 +96,5 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, AddMomentActivity::class.java))
         }
     }
+
 }
--- app/src/main/java/com/example/limoapp/adapter/ListMomentAdapter.kt
+++ app/src/main/java/com/example/limoapp/adapter/ListMomentAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.limoapp.adapter
 
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.limoapp.R
 import com.example.limoapp.databinding.ItemViewBinding
 import com.example.limoapp.domain.model.DataModel
+import java.io.File
 
 class ListMomentAdapter(): RecyclerView.Adapter<ListMomentAdapter.ViewHolder>() {
     private var listData = ArrayList<DataModel>()
@@ -18,6 +21,17 @@ class ListMomentAdapter(): RecyclerView.Adapter<ListMomentAdapter.ViewHolder>() 
     }
     class ViewHolder(val binding: ItemViewBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(data: DataModel){
+            //PATH
+            val imagePath = "/sdcard/DCIM/Camera/IMG_20230326_212949.jpg" // Ubah sesuai dengan path atau alamat file gambar di perangkat Anda
+            val file = File(imagePath)
+            if (file.exists()) {
+                val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+                binding.imgActivity.setImageBitmap(bitmap)
+            } else {
+                Toast.makeText(itemView.context, "File tidak di temukan", Toast.LENGTH_SHORT).show()
+                // File tidak ditemukan
+            }
+
             binding.apply {
                 tvComment.setText(data.comment)
                 tvJam.setText(data.time)
