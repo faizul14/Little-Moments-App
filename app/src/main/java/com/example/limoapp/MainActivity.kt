9dc1cf5168f3891dbe8eac7492658b1a6d01c5eb
Faezol <fpcyber61@gmail.com>
2023-03-26 22:00:26 +0800

    Create Layout Add Moement

--- app/src/main/java/com/example/limoapp/MainActivity.kt
+++ app/src/main/java/com/example/limoapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.limoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.limoapp.ui.AddMomentActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(Intent(this, AddMomentActivity::class.java))
     }
 }
---
+++ app/src/main/java/com/example/limoapp/ui/AddMomentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.limoapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.limoapp.R
+
+class AddMomentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_moment)
+    }
+}
